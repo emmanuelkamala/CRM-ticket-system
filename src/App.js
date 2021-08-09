614adf53c3d17c304efec9097e0bed094fee4e33
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-08-09 16:35:00 +0300

    Create header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Login from './pages/login/Login';
+import DefaultLayout from './layout/DefaultLayout';
+// import Login from './pages/login/Login';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      {/* <Login /> */}
+      <DefaultLayout />
     </div>
   );
 }
---
+++ src/layout/DefaultLayout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from './partials/Footer'
+import Header from './partials/Header'
+
+function DefaultLayout() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  )
+}
+
+export default DefaultLayout
