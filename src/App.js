e903199a179533c7e388899a98ef3e2e7832eeb2
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-08-09 17:15:29 +0300

    Add layout component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
   return (
     <div className="App">
       {/* <Login /> */}
-      <DefaultLayout />
+      <DefaultLayout>
+        Hi There
+      </DefaultLayout>
     </div>
   );
 }
--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -2,14 +2,14 @@ import React from 'react'
 import Footer from './partials/Footer'
 import Header from './partials/Header'
 
-function DefaultLayout() {
+function DefaultLayout({children}) {
   return (
     <div className='default-layout'>
       <header className='Header'>
         <Header />
       </header>
       <main className='main'>
-
+        {children}
       </main>
       <footer className='footer'>
         <Footer />
