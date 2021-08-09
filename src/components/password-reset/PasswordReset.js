9701d4e0e6de83bc5478e89b009f0db3e9103605
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-08-09 15:49:46 +0300

    Add login and password reset components

--- src/components/password-reset/PasswordReset.js
+++ src/components/password-reset/PasswordReset.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
-const LoginForm = ({handleChange, email, password, handleSubmit, formSwitcher }) => {
+const PasswordReset = ({handleChange, email, handleSubmit, formSwitcher }) => {
   return (
     <Container>
       <Row>
         <Col>
-          <h1 className='text-info text-center mt-5'>Client Login</h1>
+          <h1 className='text-info text-center mt-5'>Reset Password</h1>
           <hr/>
           <Form className="p-3" autoComplete="off" onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -22,17 +22,6 @@ const LoginForm = ({handleChange, email, password, handleSubmit, formSwitcher })
               />
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control 
-                type="password" 
-                name="password"
-                placeholder="Password" 
-                value={password} 
-                onChange={handleChange} 
-                required
-              />
-            </Form.Group>
             <Button variant="primary" type="submit">
               Login
             </Button>
@@ -42,7 +31,7 @@ const LoginForm = ({handleChange, email, password, handleSubmit, formSwitcher })
       </Row> 
       <Row className="mb-5">
         <Col>
-          <a href='#!' onClick={() => formSwitcher('reset')}>Forgot Password?</a>
+          <a href='#!' onClick={() => formSwitcher('login')}>Login Now?</a>
         </Col>
       </Row>
     </Container>
@@ -50,12 +39,11 @@ const LoginForm = ({handleChange, email, password, handleSubmit, formSwitcher })
   )
 }
 
-LoginForm.propTypes = {
+PasswordReset.propTypes = {
   handleChange: PropTypes.func.isRequired,
   handleSubmit: PropTypes.func.isRequired,
   formSwitcher: PropTypes.func.isRequired,
   email: PropTypes.string.isRequired,
-  password: PropTypes.string.isRequired,
 }
 
-export default LoginForm;
+export default PasswordReset;
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,13 +1,64 @@
-import React from 'react';
+import { useState } from 'react';
 import { Card } from 'react-bootstrap';
 import LoginForm from '../../components/LoginForm/LoginForm';
+import PasswordReset from '../../components/password-reset/PasswordReset';
 import './login.css';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [formLoad, setFormLoad] = useState('login');
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    switch(name) {
+      case 'email':
+        setEmail(value)
+        break
+      case 'password':
+        setPassword(value)
+        break
+      default:
+        break
+    }
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log(email, password);
+  }
+
+  const formSwitcher = formType => {
+    setFormLoad(formType)
+  }
+
   return (
     <div className='login bg-info'>
       <Card className='card'>
-        <LoginForm/>
+        {
+          formLoad === 'login' && (
+            <LoginForm 
+              handleChange={handleChange} 
+              email = {email}
+              password = {password}
+              handleSubmit = {handleSubmit}
+              formSwitcher = {formSwitcher}
+            />
+          )
+        }
+
+        {
+          formLoad === 'reset' && (
+            <PasswordReset
+              handleChange={handleChange} 
+              email = {email}
+              handleSubmit = {handleSubmit}
+              formSwitcher = {formSwitcher}
+            />
+          )
+        }
+        
+       
       </Card>
     </div>
   )
