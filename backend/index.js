e08b4ee52e902b611588c51c5c3d8b605bc2bd6e
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-08-11 08:13:45 +0300

    Initialize backend api

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+const app = express();
+
+// API SECURITY
+app.use(helmet());
+
+// Handle Cors
+app.use(cors());
+
+// Logger
+app.use(morgan());
+
+const PORT = process.env.PORT || 5000;
+
+app.use('/', (req, res) => {
+  res.json({message: "Hi Emmanuel"})
+})
+
+app.listen(PORT, console.log(`Server started on http://localhost:${PORT}`));
